1e392c490ec86f45dc8ff65571ab0edc425caefe
Di-UI <68942869+kokatmx@users.noreply.github.com>
2024-08-21 00:26:21 +0700

    pert27-adding auth using breeze

--- tugas/laravel/VSGA_2024/routes/web.php
+++ tugas/laravel/VSGA_2024/routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -16,15 +15,17 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('home', ["title" => "Home"]);
-});
-Route::get('/galeri', function () {
-    return view('galeri', ["title" => "Galeri"]);
+    return view('welcome');
 });
 
-Route::get('/kontak', function () {
-    return view('kontak', ["title" => "Kontak"]);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('profil', [UserController::class, 'index']);
-Route::get('profil/data', [UserController::class, 'getUsers'])->name('profil.data');
+require __DIR__.'/auth.php';
--- tugas/laravel/VSGA_2024/tailwind.config.js
+++ tugas/laravel/VSGA_2024/tailwind.config.js
@@ -1,17 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
     ],
+
     theme: {
         extend: {
             fontFamily: {
-                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
     },
-    plugins: [require("daisyui")],
+
+    plugins: [forms],
 };
